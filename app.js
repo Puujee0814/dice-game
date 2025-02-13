70a8a8d60de465de23903d2aeb01fe826dbae6d1
puujee <purevsurenbatbayr7@gmail.com>
2025-02-13 21:48:08 +0800

    index.html bolon app.js iin zassan huvilbariig oruullaa

--- app.js
+++ app.js
@@ -17,9 +17,16 @@ var dice = Math.floor(Math.random()*6)+1;
 // document.querySelector('.dice').style.display ='none';
 // document.querySelector('#score-1').innerHTML = '<em>yes<em>';
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-0').textContent = 0;
 
-console.log ('шоо :' + dice );
+var dicedom = document.querySelector('.dice');
+dicedom.style.display = 'none';
+document.querySelector('.btn-roll ').addEventListener('click',function(){
+    var dice = Math.floor(Math.random()*6)+1;
+    dicedom.style.display = 'block';
+    dicedom.src = 'dice-' + dice + '.png';
+    // alert("шоо буулаа" + dice);
+});
